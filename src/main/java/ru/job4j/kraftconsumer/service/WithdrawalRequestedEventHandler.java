0cb362de200f5fe7c22399cf40bea2bf3d57edf8
AlexeyEsipov <a_esipov@list.ru>
2025-09-10 08:32:50 +0300

    init commit

---
+++ src/main/java/ru/job4j/kraftconsumer/service/WithdrawalRequestedEventHandler.java
@@ -0,0 +1,20 @@
+package ru.job4j.kraftconsumer.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import ru.job4j.core.events.WithdrawalRequestedEvent;
+
+@Component
+@KafkaListener(topics = "debit-money-topic", containerFactory = "kafkaListenerContainerFactory")
+public class WithdrawalRequestedEventHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @KafkaHandler
+    public void handle(@Payload WithdrawalRequestedEvent withdrawalRequestedEvent) {
+        LOGGER.info("Received a new withdrawal event: {} ", withdrawalRequestedEvent.getAmount());
+    }
+}
